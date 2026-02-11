b4b7ca4851cca8afa521ddb5c252a65de1a46dd6
SENODROOM <136210526+SENODROOM@users.noreply.github.com>
2026-02-11 23:46:38 +0500

    volumeCalculator testing phase

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from './pages/Home';
 import ScrollToTop from './utils/ScrollToTop';
 import ContinuityFinder from "./pages/ContinuityFinder";
 import ExtremeValueFunction from "./pages/ExtremeValueFinder";
+import VolumeCalculator from "./pages/VolumeCalculator";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/" element={<Layout body={<Home />} />} />
           <Route path="/test" element={<Layout body={<ContinuityFinder />} />} />
           <Route path="/extreme" element={<Layout body={<ExtremeValueFunction />} />} />
+          <Route path="/volumecalculator" element={<Layout body={<VolumeCalculator />} />} />
         </Routes>
       </BrowserRouter>
     </>
